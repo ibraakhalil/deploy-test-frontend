3b0944fc2ac72b12a23612f2988e1b1da4147b71
ibrahim khalil <ibkhalil5425@gmail.com>
2025-02-15 00:17:44 +0600

    deploy test start

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,18 +3,16 @@ import { z } from "zod";
 
 export const env = createEnv({
   server: {},
-
   client: {
     NEXT_PUBLIC_BASE_URL: z.string().default("http://localhost:3000"),
-    NEXT_PUBLIC_SERVER_URL: z.string().default("http://35.200.179.36:5000"),
-    NEXT_PUBLIC_GRAPHQL_SERVER: z.string().default("http://35.200.179.36:5000/graphql"),
+    NEXT_PUBLIC_SERVER_URL: z.string().default("http://140.245.31.228:5000"),
+    NEXT_PUBLIC_GRAPHQL_SERVER: z.string().default("http://140.245.31.228:5000/graphql"),
     NEXT_PUBLIC_APP_STORE_URL: z.string().url().default("https://apps.apple.com/us/app/al-hadith/id1238182914"),
     NEXT_PUBLIC_PLAY_STORE_URL: z
       .string()
       .url()
       .default("https://play.google.com/store/apps/details?id=com.ihadis.ihadis&hl=en"),
   },
-
   runtimeEnv: process.env as Record<string, string>,
 });
 

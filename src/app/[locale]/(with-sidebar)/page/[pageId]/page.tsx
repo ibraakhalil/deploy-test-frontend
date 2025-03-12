56dacbeec6081c0e06ae5221f1cbc838683a7d02
ibrahim khalil <ibkhalil5425@gmail.com>
2025-03-12 23:15:51 +0600

    test 1

--- src/app/[locale]/(with-sidebar)/page/[pageId]/page.tsx
+++ src/app/[locale]/(with-sidebar)/page/[pageId]/page.tsx
@@ -3,6 +3,13 @@ import { SurahList } from "@/components/item-list/surah-list";
 import { env } from "@/config/env";
 import { unstable_setRequestLocale } from "next-intl/server";
 
+export function generateStaticParams() {
+  const arr = new Array(604).fill(null).map((_, index) => index + 1);
+  return arr.map(n => ({
+    pageId: n.toString(),
+  }));
+}
+
 export default async function Page({ params: { locale, pageId } }: { params: { locale: string; pageId: string } }) {
   unstable_setRequestLocale(locale);
   const res = await fetch(`${env.NEXT_PUBLIC_SERVER_URL}/wbw/by_page/${pageId}`);
--- src/config/env.ts
+++ src/config/env.ts
@@ -5,8 +5,8 @@ export const env = createEnv({
   server: {},
   client: {
     NEXT_PUBLIC_BASE_URL: z.string().default("http://localhost:3000"),
-    NEXT_PUBLIC_SERVER_URL: z.string().default("http://140.245.31.228:5000"),
-    NEXT_PUBLIC_GRAPHQL_SERVER: z.string().default("http://140.245.31.228:5000/graphql"),
+    NEXT_PUBLIC_SERVER_URL: z.string().default("http://80.225.215.51:5000"),
+    NEXT_PUBLIC_GRAPHQL_SERVER: z.string().default("http://80.225.215.51:5000/graphql"),
     NEXT_PUBLIC_APP_STORE_URL: z.string().url().default("https://apps.apple.com/us/app/al-hadith/id1238182914"),
     NEXT_PUBLIC_PLAY_STORE_URL: z
       .string()
